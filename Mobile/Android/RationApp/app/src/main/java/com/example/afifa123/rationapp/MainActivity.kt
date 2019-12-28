b8aa8e8a08d75b756a22f5a92c0cdba3968c8f50
Shamsunnahar Afifa <shamsunnaharafifa@gmail.com>
2019-12-28 21:10:38 +0600

    app init

---
+++ Mobile/Android/RationApp/app/src/main/java/com/example/afifa123/rationapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.afifa123.rationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
